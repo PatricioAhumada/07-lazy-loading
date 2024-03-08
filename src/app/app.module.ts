9d799c2acfb65271b31f488a978a146fa65325f1
Patricio Ahumada <patricio.ahumada@gmail.com>
2024-03-07 23:14:15 -0300

    Cambios finales previos al crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -12,12 +13,13 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
     SharedModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { Hero } from '../interfaces/heroe.interface';
+import { environments } from '../../../environments/environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+    private baseURL: string = environments.baseURL;
+    constructor( private http : HttpClient) {}
+
+    getHeroes():Observable<Hero[]>{
+      return this.http.get<Hero[]>(`${ this.baseURL }/heroes`);
+    }
+
+    getHeroById(id:string):Observable<Hero | undefined>{
+      return this.http.get<Hero>(`${ this.baseURL }/heroes/${id}`)
+      .pipe(
+        catchError( error => of(undefined) )
+      )
+    }
+
+    getSuggestions( query: string ):Observable<Hero[]>{
+      return this.http.get<Hero[]>(`${ this.baseURL }/heroes?q=${ query }&_limit=6`);
+    }
+}
+
